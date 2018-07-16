e2a3f8c0551e0f35c1afe09f9276545f7a1505de
Mikhail Yakshin <greycat@altlinux.org>
2018-07-16 09:49:57 +0100

    Ruby lz4: use proper lz4 library

--- ruby/lib/kaitai/compress/lz4.rb
+++ ruby/lib/kaitai/compress/lz4.rb
@@ -1,10 +1,11 @@
-require 'lz4-ruby'
+# RubyGems: extlz4
+require 'extlz4'
 
 module Kaitai
   module Compress
     class Lz4
       def decode(data)
-        LZ4::decompress(data)
+        LZ4::decode(data)
       end
     end
   end
